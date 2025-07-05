fedc2311d0dab1d86c447d057b0f98acc1b9c091
aureliolfdez <aurelio.lfdez@gmail.com>
2025-07-05 19:51:09 +0200

    Modificacion del script 2 Enrichment

--- Biclustering/GSE222045/2_Enrichment_biclusters.r
+++ Biclustering/GSE222045/2_Enrichment_biclusters.r
@@ -1,3 +1,16 @@
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("fgsea")
+BiocManager::install("org.Hs.eg.db", force=TRUE)
+BiocManager::install("hgu133plus2.db", force=TRUE)
+BiocManager::install("DOSE",force=TRUE)
+BiocManager::install("enrichplot",force=TRUE)
+BiocManager::install("clusterProfiler",force=TRUE)
+
+
+install.packages(c("ggplot2","readr"))
+
 # Cargar librerías
 library(clusterProfiler)
 library(org.Hs.eg.db)
@@ -9,9 +22,10 @@ library(readr)
 
 # Establecer el directorio
 setwd("C:/Users/marcr/OneDrive/Escritorio/IABioMed/Biclusters UniBic/GSE222045")
+setwd("/home/principalpc/git-repositories/bicNetIABiomed2025/Biclustering/GSE222045") # Path Aurelio
 
 # Leer el CSV
-biclusters <- read.csv("3_Filtrados_elite/FC_1/F1normal_DEGs_symbols_biclusters_elite.csv", stringsAsFactors = FALSE)
+biclusters <- read.csv("3_Filtrados_elite/normal/FC_3/FC3_normal_DEGs_symbols_deduplicated_biclusters_elite.csv", stringsAsFactors = FALSE)
 
 # Crear carpeta de resultados
 #dir.create("resultados_enrichment", showWarnings = FALSE)
@@ -21,7 +35,6 @@ resumen <- data.frame(
   Bicluster = character(),
   Min_GO_qvalue = numeric(),
   Top_GO_term = character(),
-  Min_KEGG_qvalue = numeric(),
   stringsAsFactors = FALSE
 )
 
@@ -31,50 +44,45 @@ for (i in 1:nrow(biclusters)) {
   genes_raw <- biclusters$Genes[i]
   genes <- unlist(strsplit(genes_raw, ";"))
   genes <- trimws(genes)
-
+  
   # Mapear a ENTREZ
   entrez_ids <- mapIds(hgu133plus2.db, keys = genes, column = "ENTREZID", keytype = "SYMBOL", multiVals = "first")
   entrez_ids <- na.omit(entrez_ids)
-
+  
   # Enriquecimiento GO
   go <- enrichGO(entrez_ids, OrgDb = org.Hs.eg.db, ont = "ALL", pAdjustMethod = "BH")
   if (!is.null(go) && nrow(go@result) > 0) {
     min_go <- min(go@result$p.adjust, na.rm = TRUE)
     top_go <- go@result$Description[which.min(go@result$p.adjust)]
-    write.csv(go@result, paste0("4_resultados_enrichment/FC_1/Bicluster_", id, "_GO.csv"), row.names = FALSE)
+    write.csv(go@result, paste0("4_resultados_enrichment/normal/FC_3/Bicluster_", id, "_GO.csv"), row.names = FALSE)
+    
+    plot_go <- dotplot(go, split = "ONTOLOGY")
+    
+    # Verificar que 'ONTOLOGY' existe en los resultados
+    if ("ONTOLOGY" %in% colnames(go@result)) {
+      plot_go <- plot_go + facet_grid(ONTOLOGY ~ ., scale = "free", space = "free")
+    }
+    
     ggsave(
-      paste0("4_resultados_enrichment/FC_1/Bicluster_", id, "_GO_plot.pdf"),
-      dotplot(go, split = "ONTOLOGY") + facet_grid(ONTOLOGY ~ ., scale = "free", space = "free")
+      paste0("4_resultados_enrichment/normal/FC_3/Bicluster_", id, "_GO_plot.pdf"),
+      plot_go
     )
   } else {
     min_go <- NA
     top_go <- NA
   }
-
-  # Enriquecimiento KEGG
-  kegg <- enrichKEGG(entrez_ids, pAdjustMethod = "BH")
-  if (!is.null(kegg) && nrow(kegg@result) > 0) {
-    min_kegg <- min(kegg@result$p.adjust, na.rm = TRUE)
-    write.csv(kegg@result, paste0("4_resultados_enrichment/FC_1/Bicluster_", id, "_KEGG.csv"), row.names = FALSE)
-    ggsave(
-      paste0("4_resultados_enrichment/FC_1/Bicluster_", id, "_KEGG_plot.pdf"),
-      dotplot(kegg)
-    )
-  } else {
-    min_kegg <- NA
-  }
-
+  
   # Añadir a la tabla resumen
   resumen <- rbind(
     resumen,
-    data.frame(Bicluster = id, Min_GO_qvalue = min_go, Top_GO_term = top_go, Min_KEGG_qvalue = min_kegg)
+    data.frame(Bicluster = id, Min_GO_qvalue = min_go, Top_GO_term = top_go)
   )
 }
 
 # Ordenar y guardar resumen
 resumen$Min_GO_qvalue <- as.numeric(resumen$Min_GO_qvalue)
 resumen <- resumen[order(resumen$Min_GO_qvalue, na.last = TRUE), ]
-write.csv(resumen, "4_resultados_enrichment/FC_1/resumen_pvalores_biclusters.csv", row.names = FALSE)
+write.csv(resumen, "4_resultados_enrichment/normal/FC_3/resumen_pvalores_biclusters.csv", row.names = FALSE)
 
 # Mostrar el más significativo
 cat("\n📌 Bicluster más significativo (GO):\n")
--- Biclustering/GSE64763/1_filtrar_elite.py
+++ Biclustering/GSE64763/1_filtrar_elite.py
@@ -3,14 +3,15 @@ import pandas as pd
 
 # Cambiar al directorio de trabajo
 os.chdir("C:/Users/marcr/OneDrive/Escritorio/IABioMed/Biclusters UniBic/GSE64763")
+#os.chdir("/home/principalpc/git-repositories/bicNetIABiomed2025/Biclustering/GSE64763") # Ruta Aurelio
 
 # Leer lista de genes élite
 with open("3_Filtrados_elite/genes_elite.txt", "r") as f:
     elite_set = set(line.strip().upper() for line in open("3_Filtrados_elite/genes_elite.txt") if line.strip())
-
+    
 # Función modificada: conserva biclusters completos si al menos uno de sus genes está en la lista de élite
 def conservar_biclusters_si_alguno_elite(input_path, elite_set, output_suffix="_elite"):
-    df = pd.read_csv(input_path)
+    df = pd.read_csv(input_path)    
 
     def contiene_gen_elite(genes_str):
         genes = [g.strip().upper() for g in genes_str.split(";")]
@@ -22,8 +23,7 @@ def conservar_biclusters_si_alguno_elite(input_path, elite_set, output_suffix="_
     df_filtrado.to_csv(output_path, index=False)
     return output_path
 
-
 # Aplicar esta versión modificada al archivo único disponible
-bicluster_file = "2_biclusters/FC_1/FC1_tumor_DEGs_symbols_deduplicated_biclusters.csv"
+bicluster_file = "2_biclusters/FC_1/FC1_normal_DEGs_symbols_deduplicated_biclusters.csv"
 output_path = conservar_biclusters_si_alguno_elite(bicluster_file, elite_set)
 print("Archivo filtrado guardado en:", output_path)
